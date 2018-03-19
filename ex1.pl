509d45eb4e1aea9b2d630349070c0969edaf49c7
gal tfilin <tfiling@post.bgu.ac.il>
2018-03-19 21:39:21 +0200

    task 3 Reverse Complement completed

--- ex1.pl
+++ ex1.pl
@@ -36,7 +36,7 @@ count(N, Word1, Word2) :-
     count(Word1, N),
     count(Word2, N).
 
-% end of recursion - anything has hamming distance of at lease 0
+% base case - anything has hamming distance of at lease 0
 innerHamming(0, _, _).
 
 % a case where the head is identical
@@ -47,12 +47,45 @@ innerHamming(N, [H1 | T1], [H2 | T2]) :-
 % a case where the head is not identical
 innerHamming(N, [H1 | T1], [H2 | T2]) :-
     H1 \== H2,
-    Z is N - 1,
-    innerHamming(Z, T1, T2).
+    Z is N - 1,                 
+    innerHamming(Z, T1, T2).    % found one char that not equal - find out if there are other N - 1 non identical chars
 
+% the actual function
 hamming(N, Word1, Word2) :-
     count(N, Word1, Word2),
-    Z is N / 2,
-    innerHamming(Z, Word1, Word2).
+    Z is N / 2,                     
+    innerHamming(Z, Word1, Word2).  % look for at least N / 2 chars that are different
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% reverse_complement
+
+% copied from https://stackoverflow.com/questions/19471778/reversing-a-list-in-prolog
+% reverse function's base case
+reverse([],Z,Z).
+
+% push back the head to the accumulator
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+
+% took insparation from https://stackoverflow.com/questions/5850937/prolog-element-in-lists-replacement
+% base case - empty lists apply the replacement
+replace(_, _, [], []).
+
+% the the first element in the first list is a replacment of the one in the second list and vice versa
+replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
+replace(O, R, [R|T], [O|T2]) :- replace(O, R, T, T2).
+
+% do not replace if the current head elements are not the onse the should be replace
+replace(O, R, [H|T], [H|T2]) :- H \= O, H \= R, replace(O, R, T, T2).
+
+% apply the replacement required for the transition from W to W^C
+replaceWord(Word, WordC) :-
+    replace('A', 'T', Word, Tmp),
+    replace('C', 'G', Tmp, WordC).
+
+
+reverse_complement(N, Word1, Word2) :-
+    reverse(Word1, Word1R, []),     % reverse Word1 to become w1^R
+    replaceWord(Word2, Word2C),     % apply the replacements converting Word2 to become W2^C
+    hamming(N, Word1R, Word2C).     % check for hamming distance as part of reverse complement's definition
 
