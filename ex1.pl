72ff7e7f0ced86c5e48785ccf68afe0ee387eed8
Niv Lipetz <niv.lipetz@zooz.com>
2018-03-27 12:47:16 +0300

    fix task 6

--- ex1.pl
+++ ex1.pl
@@ -238,16 +238,24 @@ list_of_all_acgt_permutations(N, AllPerms) :-
     list_of_all_acgt_permutations(N, [['A'], ['C'], ['G'], ['T']], [], AllPerms).
 
 list_of_all_acgt_permutations(N, [H|T], BuildList, AllPerms) :-
+    N > 1,
     append(['A'], H, L1),
     append(['C'], H, L2),
     append(['G'], H, L3),
     append(['T'], H, L4), %create all possible combinations of ACGT words
     list_of_all_acgt_permutations(N, T, [L1, L2, L3, L4|BuildList], AllPerms). %add created combinations to list already built 
 
-%stop condition when iterated over all ACGT chars and therefore list is empty
-list_of_all_acgt_permutations(_, [], BuildList, AllPerms) :-  %finished going throw all chars in [['A'], ['C'], ['G'], ['T']]
-    BuildList = AllPerms.
- 
+list_of_all_acgt_permutations(N, [], BuildList, AllPerms) :-  %finished going throw all chars in [['A'], ['C'], ['G'], ['T']]
+    N > 1,
+    N1 is N - 1,
+    list_of_all_acgt_permutations(N1, BuildList, [], AllPerms).
+
+%stop condition when the above function reaches N=1
+list_of_all_acgt_permutations(N, FinishedList, [], AllPerms) :-  
+    N = 1, 
+    FinishedList = AllPerms.
+    
+     
 %%%%%%%%%%%%%%%%%%
 % increment
 
